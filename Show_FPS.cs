08ae647e38f7a41648752c2f45bba2131b135db1
dimaTidev <timofeenkodima@gmail.com>
2021-10-28 22:23:44 +0200

    Optimize

--- Show_FPS.cs
+++ Show_FPS.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Show_FPS : MonoBehaviour
 {
     float deltaTime = 0.0f;
-    public Text fpsText;
-    [SerializeField] int targetFPS = 60;
+    [SerializeField] Text fpsText = null;
+    [SerializeField] bool isShowFullInfo = false;
+    [SerializeField] bool isOverrideFPS = false;
+    [SerializeField] int overrideFPS = 60;
 
-    string tempFPS;
+    StringBuilder sb = new StringBuilder();
+    string ms;
 
     [SerializeField] GUI_Settings guiSettings = new GUI_Settings();
 
     [System.Serializable]
-
     public class GUI_Settings
     {
-        [SerializeField] bool isDrawGUIText = false;
+        public bool isDrawGUIText = false;
         public enum HorizontalPosition { Left, Middle, Right }
         public enum VerticalPosition { Up, Middle, Down }
 
@@ -46,21 +49,62 @@ public class Show_FPS : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = targetFPS;
+        if(isOverrideFPS)
+            Application.targetFrameRate = overrideFPS;
     }
 
     void Update()
     {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
-        tempFPS = "FPS: " + Mathf.Ceil(fps).ToString();
+        if (Time.timeScale != 1) return;
 
-        if (fpsText)
-            fpsText.text = tempFPS;
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f; //deltaTime
+        float fps = 1.0f / deltaTime;
+
+
+        if (isShowFullInfo)
+        {
+            float msec = deltaTime * 1000.0f;
+            msec = (Mathf.RoundToInt(msec * 100)) / 100f;
+            sb.Clear();
+            sb.Append((int)fps).Append(" fps - ").Append(msec.ToString("0.00")).Append(" ms");
+        }
+        else
+        {
+            string ms = ((int)fps).ToString();
+            if (!ms.Equals(this.ms))
+            {
+                this.ms = ms;
+                sb.Clear();
+                sb.Append((int)fps).Append(" fps");
+            }
+        }
+
+        if(fpsText)
+            fpsText.text = sb.ToString();
     }
+
 
     void OnGUI()
     {
-        guiSettings.OnGUI(tempFPS);
+        guiSettings.OnGUI(sb.ToString());
     }
+
+
+    #region ExternalCalls
+    //-------------------------------------------------------------------------------------------------
+    public void Set_ShowFPSText(bool isEnable)
+    {
+        if (fpsText)
+        {
+            fpsText.gameObject.SetActive(isEnable);
+            fpsText.enabled = isEnable;
+        }
+    }
+
+    public void Set_ShowFPSGUI(bool isEnable)
+    {
+        guiSettings.isDrawGUIText = isEnable;
+    }
+    //-------------------------------------------------------------------------------------------------
+    #endregion
 }
